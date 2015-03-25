7376a5bf85e3d7e02a5cf915887d4a5f24d7c4e2
varvelworld <varvelworld@gmail.com>
2015-03-25 15:00:49 +0800

    update

--- Demo1/Demo1.cpp
+++ Demo1/Demo1.cpp
@@ -1,5 +1,6 @@
 #include <GLTools.h>            // OpenGL toolkit
 #include <GLShaderManager.h>    // Shader Manager Class
+#include <math3d.h>
 
 #ifdef __APPLE__
 #include <glut/glut.h>          // OS X version of GLUT
@@ -12,7 +13,11 @@ GLShaderManager	shaderManager;
 
 GLfloat vVerts[] = { -0.5f, 0.0f, 0.0f, 
 		              0.5f, 0.0f, 0.0f,
-				      0.0f, 1.0f, 0.0f };
+				      0.0f, 0.5f, 0.0f };
+
+GLfloat vColors[] = { 1.0f, 0.0f, 0.0f, 1.0f, 
+		              0.0f, 1.0f, 0.0f, 1.0f,
+				      0.0f, 0.0f, 1.0f, 1.0f };
 
 // Window has changed size, or has just been created. In either case, we need
 // to use the window dimensions to set the viewport and the projection matrix.
@@ -23,8 +28,8 @@ void ChangeSize(int w, int h) {
 // This function does any needed initialization on the rendering context. 
 // This is the first opportunity to do any OpenGL related tasks.
 void SetupRC() {
-	// Blue background
-	glClearColor(0.0f, 0.0f, 1.0f, 1.0f );
+
+    glClearColor(0.7f, 0.7f, 0.7f, 1.0f );
     
 	shaderManager.InitializeStockShaders();
 
@@ -32,6 +37,7 @@ void SetupRC() {
 	
 	triangleBatch.Begin(GL_TRIANGLES, 3);
 	triangleBatch.CopyVertexData3f(vVerts);
+    triangleBatch.CopyColorData4f(vColors);
 	triangleBatch.End();
 }
 
@@ -39,9 +45,9 @@ void SetupRC() {
 void RenderScene(void) {
 	// Clear the window with current clearing color
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-	GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-	shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
+    M3DMatrix44f identityMatrix;
+    m3dLoadIdentity44(identityMatrix);
+    shaderManager.UseStockShader(GLT_SHADER_SHADED, identityMatrix);
 	triangleBatch.Draw();
 
 	// Perform the buffer swap to display back buffer
